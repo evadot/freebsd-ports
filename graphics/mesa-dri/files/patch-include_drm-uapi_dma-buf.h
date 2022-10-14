97cfe9f3c3c3df46c9bc0ee49b919e9251b53246
Emmanuel Vadot <manu@FreeBSD.org>
2022-10-14 16:22:54 +0200

    mesa: Fix dma-buf patch

--- graphics/mesa-dri/files/patch-include_drm-uapi_dma-buf.h
+++ graphics/mesa-dri/files/patch-include_drm-uapi_dma-buf.h
@@ -1,6 +1,6 @@
---- include/drm-uapi/dma-buf.h.orig	2022-10-14 11:43:33 UTC
+--- include/drm-uapi/dma-buf.h.orig	2022-10-14 14:21:26 UTC
 +++ include/drm-uapi/dma-buf.h
-@@ -0,0 +1,182 @@
+@@ -0,0 +1,195 @@
 +/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 +/*
 + * Framework for buffer objects that can be shared across devices/subsystems.
@@ -23,7 +23,20 @@
 +#ifndef _DMA_BUF_UAPI_H_
 +#define _DMA_BUF_UAPI_H_
 +
++#ifdef __linux__
 +#include <linux/types.h>
++#else
++#include <sys/types.h>
++
++typedef int8_t   __s8;
++typedef uint8_t  __u8;
++typedef int16_t  __s16;
++typedef uint16_t __u16;
++typedef int32_t  __s32;
++typedef uint32_t __u32;
++typedef int64_t  __s64;
++typedef uint64_t __u64;
++#endif
 +
 +/**
 + * struct dma_buf_sync - Synchronize with CPU access.
