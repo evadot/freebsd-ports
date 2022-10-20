8c17993aee4cec7f7a1b7eca12e3304d34161895
Emmanuel Vadot <manu@FreeBSD.org>
2022-10-20 14:21:48 +0200

    Revert "www/firefox: Update pathname patch (2)"
    This reverts commit 45a4acb68b0b07b6454f999906e597b1d16b08f3.

--- www/firefox/files/patch-xpcom_build_BinaryPath.h
+++ www/firefox/files/patch-xpcom_build_BinaryPath.h
@@ -1,14 +1,6 @@
 --- xpcom/build/BinaryPath.h.orig	2022-10-10 16:00:29 UTC
 +++ xpcom/build/BinaryPath.h
-@@ -20,6 +20,7 @@
- #if defined(__FreeBSD__) || defined(__DragonFly__) || \
-     defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__)
- #  include <sys/sysctl.h>
-+#  include <sys/stat.h>
- #endif
- #if defined(__OpenBSD__)
- #  include <sys/stat.h>
-@@ -188,6 +189,9 @@ class BinaryPath {
+@@ -188,6 +188,9 @@ class BinaryPath {
      mib[2] = KERN_PROC_PATHNAME;
      mib[3] = -1;
  #  endif
@@ -18,7 +10,7 @@
  
      size_t len = MAXPATHLEN;
      if (sysctl(mib, 4, aResult, &len, nullptr, 0) < 0) {
-@@ -218,6 +222,11 @@ class BinaryPath {
+@@ -218,6 +221,11 @@ class BinaryPath {
      return GetFromArgv0(argv[0], aResult);
    }
  
@@ -30,7 +22,7 @@
    static nsresult GetFromArgv0(const char* aArgv0, char aResult[MAXPATHLEN]) {
      struct stat fileStat;
      // 1) use realpath() on argv[0], which works unless we're loaded from the
-@@ -256,9 +265,6 @@ class BinaryPath {
+@@ -256,9 +264,6 @@ class BinaryPath {
      }
      return NS_ERROR_FAILURE;
    }
